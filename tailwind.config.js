5dbe3c75bf3d37ce3accd6c791667d1830d74304
Hamed <hamedpyn@gmail.com>
2025-10-01 03:14:15 +0330

    Setup project with React, Tailwind CSS, and React Router

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+export default {
+  darkMode: "class", // if you use dark: classes
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}" // make sure all component files are inside src
+  ],
+  theme: { extend: {} },
+  plugins: [],
+}
