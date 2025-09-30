83b6b7721641ec9b5994ae24577686da504d0a8b
Hamed <hamedpyn@gmail.com>
2025-10-01 03:17:17 +0330

    add routes file and configure routing in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import { useRoutes } from 'react-router-dom'
 import './App.css'
+import routes from "./routes"
 function App() {
   return (
+      <div className="flex flex-col items-center bg-[#F3F4F6]">
+        {router}
+      </div>
   )
 }
 
