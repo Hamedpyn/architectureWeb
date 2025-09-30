8a40b3439fe046b027ac336206f0a17f70609a7e
Hamed <hamedpyn@gmail.com>
2025-10-01 03:23:24 +0330

    Global state management with Context for menu

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,28 @@ import { useRoutes } from 'react-router-dom'
 import './App.css'
 import routes from "./routes"
 import Context from "./Context/Context"
+import { useState } from 'react'
 
 function App() {
+  const [isSideBar, setIsSideBar] = useState(false)
+  const [openUserMenu, setOpenUserMenu] = useState(false)
+  const [isValue, setIsValue] = useState(false);
 
   const router = useRoutes(routes)
 
   return (
     <Context.Provider value={{
+      setIsSideBar,
+      isSideBar,
+      isValue,
+      setIsValue,
+      openUserMenu,
+      setOpenUserMenu,
     }}>
       <div className="flex flex-col items-center bg-[#F3F4F6]">
         {router}
       </div>
+    </Context.Provider>
   )
 }
 
