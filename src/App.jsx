177ad213f14b24097668a326891f0d42d59290c1
Hamed <hamedpyn@gmail.com>
2025-10-01 03:21:37 +0330

    create initial app context

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import { useRoutes } from 'react-router-dom'
 import './App.css'
 import routes from "./routes"
+import Context from "./Context/Context"
+
 function App() {
+
+  const router = useRoutes(routes)
+
   return (
+    <Context.Provider value={{
+    }}>
       <div className="flex flex-col items-center bg-[#F3F4F6]">
         {router}
       </div>
